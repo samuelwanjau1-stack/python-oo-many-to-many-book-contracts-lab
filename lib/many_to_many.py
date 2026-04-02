51134ce4c538b019fb04e98cfa92a51d57fc62ba
Your Full Name <samuel.wanjau1@student.moringaschool.com>
2026-04-02 19:11:52 +0300

    Completed many-to-many relationship logic

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -2,14 +2,11 @@ class Author:
     all = []
 
     def __init__(self, name):
+        # Validation to ensure name is a string
         if not isinstance(name, str) or len(name) == 0:
             raise Exception("Name must be a non-empty string")
-        self._name = name
+        self.name = name
         Author.all.append(self)
-
-    @property
-    def name(self):
-        return self._name
 
     def contracts(self):
         return [c for c in Contract.all if c.author == self]
@@ -18,7 +15,6 @@ class Author:
         return list(set([c.book for c in self.contracts()]))
 
     def sign_contract(self, book, date, royalties):
-       
         return Contract(self, book, date, royalties)
 
     def total_royalties(self):
@@ -31,12 +27,8 @@ class Book:
     def __init__(self, title):
         if not isinstance(title, str) or len(title) == 0:
             raise Exception("Title must be a non-empty string")
-        self._title = title
+        self.title = title
         Book.all.append(self)
-
-    @property
-    def title(self):
-        return self._title
 
     def contracts(self):
         return [c for c in Contract.all if c.book == self]
@@ -49,15 +41,15 @@ class Contract:
     all = []
 
     def __init__(self, author, book, date, royalties):
-        # Validation checks required 
+        # Validation checks
         if not isinstance(author, Author):
-            raise Exception("Author must be an instance of Author class")
+            raise Exception("Invalid author")
         if not isinstance(book, Book):
-            raise Exception("Book must be an instance of Book class")
+            raise Exception("Invalid book")
         if not isinstance(date, str):
-            raise Exception("Date must be a string")
+            raise Exception("Invalid date")
         if not isinstance(royalties, int):
-            raise Exception("Royalties must be an integer")
+            raise Exception("Invalid royalties")
 
         self.author = author
         self.book = book
