e17914259d4c239d1e158442bb851c482b069e01
Your Full Name <samuel.wanjau1@student.moringaschool.com>
2026-04-02 19:09:21 +0300

    Completed many-to-many relationship logic

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -15,11 +15,10 @@ class Author:
         return [c for c in Contract.all if c.author == self]
 
     def books(self):
-        # Uses set() to ensure we don't return the same book twice 
-        # if there are multiple contracts for one book.
         return list(set([c.book for c in self.contracts()]))
 
-    def sign_contracts(self, book, date, royalties):
+    def sign_contract(self, book, date, royalties):
+       
         return Contract(self, book, date, royalties)
 
     def total_royalties(self):
@@ -50,15 +49,15 @@ class Contract:
     all = []
 
     def __init__(self, author, book, date, royalties):
-        # Validation checks as requested in Task 3 Step 4
+        # Validation checks required 
         if not isinstance(author, Author):
-            raise Exception("author must be an instance of Author")
+            raise Exception("Author must be an instance of Author class")
         if not isinstance(book, Book):
-            raise Exception("book must be an instance of Book")
+            raise Exception("Book must be an instance of Book class")
         if not isinstance(date, str):
-            raise Exception("date must be a string")
+            raise Exception("Date must be a string")
         if not isinstance(royalties, int):
-            raise Exception("royalties must be an integer")
+            raise Exception("Royalties must be an integer")
 
         self.author = author
         self.book = book
